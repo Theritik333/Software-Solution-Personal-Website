8b2c76bef69b6fd8e7d24a06a0032b3d1c01ed6e
Therithik333 <rameshwark072@gmail.com>
2026-06-27 14:19:30 +0530

    Update frontend URL for Problem

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://software-solution-personal-website.onrender.com",
+  baseURL: "https://software-solution-personal-website.onrender.com/api",
   withCredentials: true,
 });
 
