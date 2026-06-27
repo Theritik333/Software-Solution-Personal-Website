3631d02b251c0a4c5eda76962525000e3a8f01d7
Therithik333 <rameshwark072@gmail.com>
2026-06-27 13:39:43 +0530

    Add VITE_API_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,6 +7,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+  
 });
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 export default api;
